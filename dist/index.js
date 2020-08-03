1bd03f1474908ee30c873eadab54af3487fa6edc
George Kalpaktsoglou <kalpgiorgos@gmail.com>
2020-08-03 15:00:41 +0300

    new files added

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// ES5
+for (var i = 1; i < 5; i++) {
+  console.log(i);
+}
+//  <-- logs the numbers 1 to 4
+console.log(i);
---
+++ src/index.js
@@ -0,0 +1,6 @@
+// ES5
+for (var i = 1; i < 5; i++) {
+  console.log(i);
+}
+//  <-- logs the numbers 1 to 4
+console.log(i);
